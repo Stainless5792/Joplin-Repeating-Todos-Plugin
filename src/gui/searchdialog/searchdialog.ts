dcc34f3d7d39da14d678cf6543cb5f5be43d59a6
kyma <kyma@hillinsight.com>
2025-07-01 17:48:49 +0800

    update: add timestamp in search result

--- src/gui/searchdialog/searchdialog.ts
+++ src/gui/searchdialog/searchdialog.ts
@@ -68,10 +68,29 @@ export async function openSearchDialog(){
             const todo_result = result.filter(item => item.is_todo !== 0).sort((a, b) => a.title.localeCompare(b.title))
             const completed_todo_result = todo_result.filter(item => item.todo_completed !== 0).sort((a, b) => a.title.localeCompare(b.title))
             const incompleted_todo_result = todo_result.filter(item => item.todo_completed == 0).sort((a, b) => a.title.localeCompare(b.title))
+            const timestamp = Date.now();
+            const date = new Date(timestamp);
+            const year = date.getFullYear();
+            const month = ('0' + (date.getMonth() + 1)).slice(-2); // 月份是从0开始的
+            const day = ('0' + date.getDate()).slice(-2);
+            const hours = ('0' + date.getHours()).slice(-2);
+            const minutes = ('0' + date.getMinutes()).slice(-2);
+            const seconds = ('0' + date.getSeconds()).slice(-2);
+            // 获取星期几
+            const dayOfWeek = date.getDay();
+            const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            const dayOfWeekName = week[dayOfWeek];
+
+            // 格式化时间戳
+            const formattedTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${dayOfWeekName}`;
+
+            // console.log(formattedTimestamp);
+
+
             // console.log(`result: ` + JSON.stringify(result))
 
             const search_String = `- Search Title With(Use Regex): '` + formResult.formData.searchForm.searchText + `'`
-            const Total_String = search_String + `\n- Fount Total Count: ` + result.length + `\n- Note Result: ` + note_result.length + `\n- Todo Result: ` + todo_result.length + `\n- Completed Todo Result: ` + completed_todo_result.length + `\n- Incompleted Todo Result: ` + incompleted_todo_result.length
+            const Total_String = search_String + `\n- Fount Total Count: ` + result.length + `\n- Note Result: ` + note_result.length + `\n- Todo Result: ` + todo_result.length + `\n- Completed Todo Result: ` + completed_todo_result.length + `\n- Incompleted Todo Result: ` + incompleted_todo_result.length + `\n- Execution Time: ` + formattedTimestamp
             const copy_String = `Click 'OK' tp Copy Result to Clipboard`
             // const show_Message = Total_String + "\n\n" + copy_String
             
